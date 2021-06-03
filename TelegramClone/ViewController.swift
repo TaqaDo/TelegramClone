cff249e19791aabc68f116f9d585a3538a07a586
TaqaDo <osmonov1337@gmail.com>
2021-06-03 21:46:20 +0600

    Initial Commit

---
+++ TelegramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelegramClone
+//
+//  Created by talgar osmonov on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
